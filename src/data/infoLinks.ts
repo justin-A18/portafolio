273783a9c000ff9688cd60bb16288eb0e327d87d
Justin Huertas <justin.hv08@gmail.com>
2024-08-18 21:07:13 -0500

    terminar vista principal

---
+++ src/data/infoLinks.ts
@@ -0,0 +1,14 @@
+export const infoLinks = [
+	{
+		id: 0,
+		name: 'Contactame',
+		boxIcon: 'bx-envelope',
+		href: "mailto:justin.hv08@gmail.com",
+	},
+	{
+		id: 1,
+		name: 'Linkedin',
+		boxIcon: 'bxl-linkedin',
+		href: 'https://www.linkedin.com/in/dev-justinh/',
+	}
+];
---
+++ src/data/navLinks.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+	{
+		id: 0,
+		name: 'Experiencia',
+		href: '/#experiencia',
+	},
+	{
+		id: 1,
+		name: 'Proyectos',
+		href: '/#proyectos',
+	},
+	{
+		id: 2,
+		name: 'Sobre mí',
+		href: '/#Sobremi',
+	},
+	{
+		id: 3,
+		name: 'Contacto',
+		href: '/#contacto',
+	}
+]

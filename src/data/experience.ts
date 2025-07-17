a5c07f1dac65f171fa9e34d2de6146707e16095d
Justin Huertas <justin.hv08@gmail.com>
2025-07-17 11:51:05 -0500

    refactor: update experience and link of cv

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,16 +3,16 @@ import { type Experience } from '../types';
 export const experience: Experience[] = [
 	{
 		id: 0,
-		date: 'Marzo 2022 - Actualmente',
-		title: 'Ingeniería de Sistemas - 2do año',
+		date: 'Octubre 2024 - Diciembre 2024',
+		title: 'FractalUp - Desarrollador FrontEnd (Remoto)',
 		description:
-			'Estoy inmerso en mi segundo año de Ingeniería de Sistemas, donde destaco por mi enfoque proactivo en la resolución de problemas y la aplicación práctica de conceptos avanzados. Mi pasión por la tecnología se refleja en mi compromiso constante con el aprendizaje y la búsqueda de soluciones innovadoras en el ámbito de la ingeniería de sistemas.',
+			'Mejoré la UX/UI de módulos clave, elevando la calidad del producto. Optimizé el rendimiento con código limpio y escalable. Implementé i18n, lógica condicional y funciones admin-friendly que aumentaron la flexibilidad de la plataforma.',
 	},
 	{
 		id: 1,
-		date: 'Enero 2022 - Actualmente',
-		title: 'Desarrollo Frontend Autodidacta',
+		date: 'Marzo 2025 - Actualidad',
+		title: 'OpenLab Latam - Desarrollador FrontEnd (Remoto)',
 		description:
-			'Con más de un año de experiencia autodidacta en desarrollo frontend, he contribuido a proyectos centrados en crear experiencias de usuario memorables y efectivas. Mi enfoque se centra en la innovación y la eficiencia para ofrecer soluciones que mejoren significativamente la experiencia del usuario.',
+			'Construí interfaces robustas desde cero con enfoque modular. Integré formularios avanzados, carga de archivos y validaciones reactivas. Trabajo colaborativo con equipos multidisciplinarios, asegurando entregas ágiles y de alto impacto.',
 	},
 ];
